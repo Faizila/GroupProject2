f110993b9fc3d1224e4d68875cf6f689b7f21650
chris-noriega14 <chris.noriega14@hotmail.com>
2021-10-29 19:12:40 -0500

    Finished setting up Exercise and Login Models

---
+++ models/Login.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Login extends Model {}
+
+Login.init(
+    {
+        id : {
+        type: DataTypes.INTEGER,
+        autoincrement: true,
+        allowNull: false,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            primarykey: true,
+            isEmail: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        sequelize,
+        model: 'login'
+    }
+
+    )

8b1009e9b4b9888c069ce51dc047670c8f4800fd
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-03 22:11:45 -0500

    added my list functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,17 +1,34 @@
 const router = require('express').Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const { UserExercises } = require('../../models');
+const session = require('express-session');
+
+// add to user's list
+router.post('/add', async (req, res) => {
+    try {
+        // console.log(req.session)
+        // find all exercises .findAll
+        // "exercise_id": "4",
+// "login_email": "req.sesson" 
+// const test = req.body + "login_email" + ' + session.login.email
+        const addEx = await UserExercises.create(test);
+          
+
+        console.log("=========================================================================================")
+
+        
+        const myExercises =  test.map(e => e.get({ plain: true }));
+console.log(myExercises)
+        // success
+        // stretch.handlebars
+        res.render('mylist', {myExercises, loggedIn: req.session.loggedIn})
 
 
-//  load the list of exercises of the users
-// router.post('/addexercise', ensureAuthenticated , async (req, res) => {
-//     try {
-
-
-
-//     } catch (err) {
-//         res.status(500).json(err);
-//       }
-//     });
+        // error
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // router.delete('/delexercise', ensureAuthenticated , async (req, res) => {
